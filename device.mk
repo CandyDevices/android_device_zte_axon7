b9ee5782ecd771f8e9898f75a8a633ce4b88c6f9
Aren Clegg <aclegg2011@gmail.com>
2017-11-06 15:59:14 -0800

    axon7: import generic imp list

--- device.mk
+++ device.mk
@@ -147,6 +147,51 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
 
+# Hidl imp and services
+PRODUCT_COPY_FILES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.bluetooth@1.0-service \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    camera.device@3.2-impl \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service\
+    android.hardware.biometrics.fingerprint@2.0-service \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    android.system.net.netd@1.0 \
+    netutils-wrapper-1.0 \
+    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    android.hardware.renderscript@1.0-impl \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
+    android.hardware.usb@1.0-service \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+    android.hardware.wifi@1.0-service
+
+
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
@@ -297,5 +342,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml
 
-
 $(call inherit-product-if-exists, vendor/zte/axon7/axon7-vendor.mk)
+
+
