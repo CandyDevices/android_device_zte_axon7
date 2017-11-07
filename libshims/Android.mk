af96ee1dd574d26d053a916a4124e9beafb295e1
Aren Clegg <aclegg2011@gmail.com>
2017-11-06 16:19:23 -0800

    axon7: update camera shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -4,8 +4,16 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := camera.cpp
-LOCAL_SHARED_LIBRARIES := libui
+
+LOCAL_SHARED_LIBRARIES := \
+    libgui \
+    libui
+
+LOCAL_C_INCLUDES := \
+    frameworks/native/include
+
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 32
 
 include $(BUILD_SHARED_LIBRARY)
