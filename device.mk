87e14ff0c5e50084f12d81f3bb1e1d038e8ca4ed
Aren Clegg <aclegg2011@gmail.com>
2017-11-06 16:14:01 -0800

    axon7: add libbt-vendor to bluetooth

--- device.mk
+++ device.mk
@@ -67,6 +67,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
+    libbt-vendor \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
